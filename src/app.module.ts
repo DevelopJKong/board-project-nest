5748bb470ea0463cd93804f79fc4497e290a5901
cafe small house <tbaram10@naver.com>
2023-02-13 23:16:06 +0900

    local middleware 설정완료

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LocalMiddleware } from './local/local.middleware';
 
 @Module({
   imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LocalMiddleware).forRoutes({
+      path: '*',
+      method: RequestMethod.ALL,
+    });
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
+import * as session from 'express-session';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    cors: true,
+  });
+  app.setBaseViewsDir(join(__dirname, '../public', 'views'));
+  app.setViewEngine('hbs');
+  app.use(
+    session({
+      secret: process.env.COOKIE_SECRET,
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        maxAge: 3600000,
+      },
+      store: require('connect-mongo').create({
+        mongoUrl: process.env.DB_URL,
+      }),
+    }),
+  );
+  const PORT = 5000;
+  await app.listen(PORT);
+  console.log(`Application is running on: 🎁 http://localhost:${PORT}`);
 }
 bootstrap();
